5a935b19a8c982a916553955adc6501ec5a6f918
zq <rxontdkd@gmail.com>
2025-09-07 16:34:27 +1000

    bug fix qwen

--- modeling/qwen25vl.py
+++ modeling/qwen25vl.py
@@ -28,7 +28,7 @@ def get_inputs_func(prompt, frames, processor):
     text = processor.apply_chat_template(
         messages, tokenize=False, add_generation_prompt=True
     )
-    image_inputs, video_inputs = process_vision_info([[messages]])
+    image_inputs, video_inputs = process_vision_info([messages])
     
     inputs = processor(
         text=[text],
